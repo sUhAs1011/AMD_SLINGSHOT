4185ecf7404989c4efe171ad5e0852c3c23f750e
Shreyas-S03 <shreyassrsh@gmail.com>
2026-03-01 22:28:05 +0530

    final cleanup

--- backend/push.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from sqlalchemy import create_engine
-
-df = pd.read_csv(r'C:\Users\NITRO\OneDrive\Desktop\Kalpana_Mental_Health\Datasets\harm_train.csv')
-engine = create_engine('postgresql://postgres:root123@localhost:5432/postgres')
-
-df.to_sql('harm_train', engine, index = False, if_exists = 'replace')
